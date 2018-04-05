dd716a31cba7fadae8966acf6be20a0ab1106a7b
rubiculite <dr_smike@yahoo.ca>
2018-04-05 16:52:04 -0500

    Housecleaning.

--- plotter.js
+++ plotter.js
@@ -9,19 +9,23 @@ function plotter(d3_AppendToElement,data) {
 
    this.pd = data;
 
+   // define plot layout
    this.xAxisPixelLength = 500;
    this.yAxisPixelLength = 400;
    this.boarder = {top: 50, right: 50, bottom: 75, left: 75};
 
+   // define axes
    this.xScale = d3.scaleLinear().domain([this.pd.xMin,this.pd.xMax]).range([0,this.xAxisPixelLength]).nice();
    this.yScale = d3.scaleLinear().domain([this.pd.yMin,this.pd.yMax]).range([this.yAxisPixelLength,0]).nice();
-
-   this.svgContainer = d3_AppendToElement.append("svg")
-      .attr("width",this.boarder.left+this.xAxisPixelLength+this.boarder.right)
-      .attr("height",this.boarder.top+this.yAxisPixelLength+this.boarder.bottom);
    this.xAxis = d3.axisBottom(this.xScale);
    this.yAxis = d3.axisLeft(this.yScale);
 
+   // create svg container
+   this.svgContainer = d3_AppendToElement.append("svg")
+      .attr("width",this.boarder.left+this.xAxisPixelLength+this.boarder.right)
+      .attr("height",this.boarder.top+this.yAxisPixelLength+this.boarder.bottom);
+
+   // create plot container
    this.svgPlotContainer = this.svgContainer.append("g").attr("class","scatter-plot")
       .attr("transform","translate("+this.boarder.left+","+this.boarder.top+")");
 
